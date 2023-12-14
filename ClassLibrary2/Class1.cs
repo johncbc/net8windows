8840d6319489d54fa326cd75f1a6a33117e1ec0e
John Cottrell <johnc@businesscraft.com.au>
2023-12-14 17:02:01 +1100

    Initial code

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static void HelloWorld()
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
